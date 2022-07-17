ecc637e3b62d1d10c389f72a78c59ff42f57df29
Connor Hanley <connordhanley@gmail.com>
2022-07-16 21:51:19 -0500

    tryna get # of nodes in the tree

--- src/parsing.c
+++ src/parsing.c
@@ -1,8 +1,75 @@
 #include "mpc.h"
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h> // standard library
 #include <editline/readline.h> // read input allowing editing
 #include <readline/history.h> // record history of inputs with up and down arrows
+
+/* Compute the number of leaves in the tree 
+ *
+ *	tree_leaves :: mpc_ast_t* -> int 
+ *	tree_leaves [] = 0 
+ *	tree_leaves (t tag children .. ) = 
+ *	  case tag of 
+ *		""		 -> + 0
+ *		"number" -> (+ 1)
+ *		_		 -> tree_leaves xs
+ *
+ * */ 
+int tree_leave(mpc_ast_t* t) {
+	int leaves = 0;
+	if (strstr(t->tag, "expr")) { return leaves++; }
+	
+	int i = 3;
+	while (strstr(t->children[i]->tag, "expr")) {
+		leaves++;
+		
+	}	
+
+	assert(leaves == t->children_num && "tree leave computed, and that contained do not match");
+	return leaves;
+}
+
+/* Use strcmp() to directly equate two strings and determine 
+ * if it is some operator */
+long eval_op(long x, char* op, long y) {
+	if (strcmp(op, "+") == 0) { return x + y; }
+	if (strcmp(op, "-") == 0) { return x - y; }
+	if (strcmp(op, "/") == 0) { return x / y; }
+	if (strcmp(op, "*") == 0) { return x * y; }
+	return 0;
+}
+
+/* Determine which operator to use, 
+ * and checking if tag conatins some substring
+ * */
+long eval(mpc_ast_t* t) {
+
+	// Checking to see if tag is number, if so return
+	// immediately
+	if (strstr(t->tag, "number")) {
+		// strstr checks for substring, think .contains()
+		return atoi(t->contents);
+	}
+
+	// Operator is always the second child
+	char* op = t->children[1]->contents;
+	//			^checks first child of node
+	//						 ^ and looks at its contents
+	
+	// Descent to the number
+	long x = eval(t->children[2]);
+
+	// Iterate over the remaining children and combining
+	int i = 3;
+	while (strstr(t->children[i]->tag, "expr")) {
+		x = eval_op(x, op, eval(t->children[i])); // recursive descent
+												  // down the tree
+		i++;
+	}
+
+	return x;
+}
 
 int main(int argc, char** argv) {
 	
@@ -19,8 +86,8 @@ int main(int argc, char** argv) {
 			number	 : /-?[0-9]+/ ;														\
 			string	 : /[ab]+/ ;														\
 			operator : '+' | '-' | '*' | '/' | '%' ;									\
-			expr	 : <string> | <number> | '(' <operator> <expr>+ ')' ;				\
-			lispy	 : /^/ <string>+ | <operator> <expr>+ /$/ ;							\
+			expr	 : <number> | '(' <operator> <expr>+ ')' ;							\
+			lispy	 : /^/ <operator> <expr>+ /$/ ;										\
 		",
 		Number, String, Operator, Expr, Lispy);
 	
@@ -34,7 +101,13 @@ int main(int argc, char** argv) {
 		/* Attempt to call and parse user input */ 
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, Lispy, &r)) {
+			long result = eval(r.output);
+			
 			mpc_ast_print(r.output);
+
+			tree_leave(r.output);
+
+			printf("%li\n", result);
 			mpc_ast_delete(r.output);
 		} else {
 			mpc_err_print(r.error);
